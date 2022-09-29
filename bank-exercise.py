f11cb0d52ae4fb66d007809eaf0fa60e424f4761
SirMeows <araelbrighteyes@gmail.com>
2022-09-29 16:53:39 +0200

    added comments

--- bank-exercise.py
+++ bank-exercise.py
@@ -15,7 +15,7 @@ class Account:
             self.customer = customer
 
     def print_account_nr(self):
-        return f'account nr: { self.account_nr }'
+        print(f'account nr: { self.account_nr }')
 
 
 class Customer:
@@ -24,28 +24,24 @@ class Customer:
         self.name = name
         self.customer_id = customer_id
 
-    def __str__(self): # prints a string (user friendly)
+    def __str__(self):
         return f'name: {self.name}, customer_id: {self.customer_id}'
 
-    def __repr__(self): # prints a dictionary (developer friendly)
-        return f'name: {self.name}, customer_id: {self.customer_id}'
+    def __repr__(self):
+        return f'Customer("{ self.name }", {self.customer_id})'
 
 bank_1 = Bank()
-print(bank_1)
 
 customer_1 = Customer('Arael', 1)
+customer_2 = Customer('Xatana', 2)
 
 account_1 = Account(12345, customer_1)
-account_2 = Account(56789, Customer('Xatana', 2))
+account_2 = Account(56789, customer_2)
 
 bank_1.add_accounts(account_1, account_2)
 
-account_1.print_account_nr
+account_1.print_account_nr()
 
-print(customer_1)
-print(account_2.customer.name)
-
-
-
-
-
+print(customer_1) # calls __str__
+print(repr(customer_1)) # string representation of the object
+print(eval(repr(customer_1))) # new object 

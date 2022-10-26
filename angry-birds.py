09ebc791830c8df7ebb17c34b586b41eeab86a48
SirMeows <araelbrighteyes@gmail.com>
2022-10-26 23:04:58 +0200

    Basically everything

--- angry-birds.py
+++ angry-birds.py
@@ -1,21 +1,21 @@
-import numpy as np
-from inspect import CORO_CREATED
-from types import coroutine
+import random
 
-# coordinates = [(x,y) for x in range(10) for y in range(10)]
 directions = ['n', 'e', 's', 'w']
-turns = ('l', 'r')
+start_orientation = 'n'
 max_x = 10
 max_y = 10
+valid_commands = ['f', 'r', 'l']
+instructions = 'You are facing {start_orientation}.\nPress f to move forward\nPress r to turn right.\nPress l to turn left.\nPress enter d when you have plotted the whole course.'
 
-def win(contestant):
+def declare_winner(contestant):
     print(f"{contestant} wins")
-
+   
 class Bird:
 
     def __init__(self, position, direction):
         self.position = position
         self.direction = direction
+        self.glyph = 'B'
 
     def __str__(self):
         return 'bird'
@@ -32,16 +32,13 @@ class Bird:
     def move_west(self):
         self.position[0] -= 1
 
-    def change_direction(self, index):
-        self.direction = index % len(directions)
-
     def turn_left(self):
         new_index = directions.index(self.direction) -1
-        self.change_direction(self, new_index)
+        self.direction = directions[new_index % len(directions)]
 
     def turn_right(self):
         new_index = directions.index(self.direction) +1
-        self.change_direction(self, new_index)
+        self.direction = directions[new_index % len(directions)]
     
     def move(self):
         if self.direction == 'n':
@@ -54,67 +51,107 @@ class Bird:
             self.direction == 'w'
             self.move_west()
 
-    def declare_winner(self, pig):
-
-        if self.position == pig.position:
-            print('bird eats the pig')
-        else:
-            print('boohoo, bird fails')
 
 class Pig:
 
     def __init__(self, position):
         self.position = position
+        self.glyph = 'P'
 
     def __str__(self):
-        return 'pig'
+        return 'pig'    
 
-""" def draw_board(bird_pos, pig_pos):
-
-    bird_symbol = 'B'
-    pig_symbol = 'P'
-
-    symbols = ['*'] * len(coordinates)
-    
-    for c in coordinates:
-
-        i = coordinates.index(c)
-        
-        if c == (bird_pos):
-            symbols[i] = bird_symbol
-        if c == pig_pos:
-            symbols[i] = pig_symbol
-   
-    splitted = np.array_split(symbols, 10)
-
-    # TODO: print only elements, not arrays
-    for arr in splitted:
-        print(arr)   """
 
 class Board:
-    # TODO: Add random start positions
-    bird = Bird((0,0), directions[0])
-    pig = Pig((9,9))
 
-    # draw_board(bird.position, pig.position)
+    def row_to_str(self, row):
+        return'\t'.join([str(cell) for cell in row])
+
+    def positions_arr(self):
+        arr = [[('*') for i in range(max_y)] for j in range(max_x)]
+        return arr
+
+    def render_animal(self, animal, board_repr ):
+        x, y = (animal.position[0], animal.position[1])
+        board_repr[y][x] = animal.glyph         
+
+    def draw_board(self, bird, pig):
+        pos_arr = self.positions_arr()
+
+        self.render_animal(pig, pos_arr)
+        self.render_animal(bird, pos_arr)
+
+        print('\n'.join(
+            [self.row_to_str(row) for row in pos_arr]
+            ))
+
 
 class Workspace:
 
-    commands = []
+    def print_instructions(self):
+        print('Try to eat the pig by moving to its square!\nchain commands to create path and then reveal theresult\n' + instructions)
 
-    def print_instructions():
-        print('try to eat the pig by moving to its square! press f to move forward, press r to turn right, press l to turn left')
+    def create_commands_list(self):
+        commands = []
+        command = input()
 
-    # def add_command(input):
-        
+        while command != 'd':
+            if not command in valid_commands:
+                print('That is not a valid command.\n' + instructions)
+            else:
+                commands.append(command)
+                command = input()
+        return commands
+     
 class Game:
 
-    # runs app
+    def __init__(self):
 
-    board = Board()
-    workspace = Workspace()
+        self.bird = Bird([self._rnd(max_x), self._rnd(max_y)], start_orientation)
+        self.pig = Pig([self._rnd(max_x), self._rnd(max_y)])   
+        self.board = Board()
+        self.workspace = Workspace()
 
+    def _rnd(self, max_size):
+        return random.randint(0, max_size)
 
-    # determines whether bird wins or loses
+    def initialize(self):
+        self.board.draw_board(self.bird, self.pig)
+        self.workspace.print_instructions()
 
-print(coordinates)
+    def play(self):
+        commands = self.workspace.create_commands_list()
+
+        for action in commands:
+            if action == 'f':
+                self.bird.move()
+            elif action == 'r':
+                self.bird.turn_right()
+            elif action == 'l':
+                self.bird.turn_left()
+            else:
+                print('error: unknown input')
+
+    def determine_winner(self):
+        self._determine_winner(self.bird, self.pig)
+
+    def _determine_winner(self, bird, pig):
+
+        winner = ''
+
+        if bird.position == pig.position:
+            winner = bird
+        elif bird.position != pig.position:
+            winner = pig
+        else:
+            print('error')
+
+        declare_winner(winner)
+
+        self.board.draw_board(bird, pig)
+
+game = Game()   
+
+game.initialize()
+game.play()
+game.determine_winner()
